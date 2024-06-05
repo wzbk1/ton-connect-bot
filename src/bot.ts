80137ab4bf44efcb363217fd98a9465da5c3a81c
wzbk1 <161694279+wzbk1@users.noreply.github.com>
2024-06-05 18:02:11 +0300

    Create bot.ts

---
+++ src/bot.ts
@@ -0,0 +1,8 @@
+// src/bot.ts
+
+import TelegramBot from 'node-telegram-bot-api';
+import * as process from 'process';
+
+const token = process.env.TELEGRAM_BOT_TOKEN!;
+
+export const bot = new TelegramBot(token, { polling: true });
